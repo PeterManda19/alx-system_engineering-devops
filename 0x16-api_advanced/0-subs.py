172bae03e58d623c8bb94fe7f2e5c53e4794ac5c
PeterManda19 <peter.pmanda.manda@gmail.com>
2023-05-10 22:32:51 +0200

    pep 8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Queries the Reddit API and returns the number of subscribers for a given subreddit.
+Queries the Reddit API and returns the number
+of subscribers for a given subreddit.
 """
 
 import requests
@@ -8,9 +9,11 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-    Returns the number of subscribers for a given subreddit.
+    Returns the number of subscribers for
+    a given subreddit.
     """
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    url = 'https://www.reddit.com/r/{}/about.json'.format(
+        subreddit)
     headers = {'User-Agent': 'My User Agent 1.0'}
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
@@ -23,6 +26,10 @@ if __name__ == '__main__':
     subreddit = input("Enter subreddit name: ")
     subscribers = number_of_subscribers(subreddit)
     if subscribers != 0:
-        print(f"Number of subscribers in {subreddit}: {subscribers}")
+        print(
+            f"Number of subscribers in {subreddit}: {subscribers}"
+            )
     else:
-        print(f"Subreddit {subreddit} not found or API error.")
+        print(
+            f"Subreddit {subreddit} not found or API error."
+            )
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
+Queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit.
 """
 
 import requests
@@ -8,20 +9,24 @@ import requests
 
 def top_ten(subreddit):
     """
-    Prints the titles of the first 10 hot posts listed for a given subreddit.
+    Prints the titles of the first 10 hot posts
+    listed for a given subreddit.
     """
     if len(sys.argv) < 2:
         print("Please enter a subreddit name")
         return
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json".format(
+        subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     try:
         response = requests.get(url, headers=headers,
                                 allow_redirects=False)
         if response.status_code == 200:
-            children = response.json().get('data').get('children')
+            children = response.json().get(
+                'data').get('children')
             for i in range(10):
-                print(children[i].get('data').get('title'))
+                print(children[i].get('data').get(
+                    'title'))
         else:
             print("None")
     except Exception:
